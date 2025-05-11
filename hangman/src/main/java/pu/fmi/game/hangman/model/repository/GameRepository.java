8c6caaa5db60ed48287d1b24956c4299dde25f59
DannnyDrago <162352165+DannnyDrago@users.noreply.github.com>
2025-05-11 17:48:09 +0100

    Update GameRepository.java

--- hangman/src/main/java/pu/fmi/game/hangman/model/repository/GameRepository.java
+++ hangman/src/main/java/pu/fmi/game/hangman/model/repository/GameRepository.java
@@ -10,7 +10,7 @@ public interface GameRepository extends JpaRepository<HangmanGame, Long> {
   // SELECT * FROM HANGMAN_GAME
   // WHERE STATUS = ?
   List<HangmanGame> findByStatus(Status status);
-
-  // TODO: Add query method to find first 10 games ordered by started on date desc
-  // Look here for examples: https://docs.spring.io/spring-data/jpa/reference/repositories/query-methods-details.html#repositories.limit-query-result
+    @Query("SELECT h FROM HangmanGame h ORDER BY h.startedOn DESC")
+    List<HangmanGame> findTop10ByOrderByStartedOnDesc();
+  
 }
